9195db5d7c424b0b5ff98169056a636157495aa9
Artyom-Voronin <72145948+Artyom-Voronin@users.noreply.github.com>
2020-10-15 17:45:37 +0200

    dop
    norm

---
+++ lab3_ex22.py
@@ -0,0 +1,9 @@
+import numpy as np
+import lab3_ex1 as physic
+T = 200 
+ε = 300
+one = (2/(np.sqrt(np.pi))) * (physic.h/((physic.k*T)**3/2))
+two = (e**-e/physic.k*T) * (e**T/2)
+N = one * two
+print (N)
+
